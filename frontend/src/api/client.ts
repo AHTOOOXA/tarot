10ea9d2677877c007503c5bf1c659178118f799c
Anton Safonov <anton.safonov.loves.cats@gmail.com>
2024-10-26 19:25:35 +0500

    forgot auth

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,9 +1,15 @@
 import createClient from "openapi-fetch";
 import type { paths } from '../types/schema';
+import useTelegram from '@/services/useTelegram';
+
+const { webAppInitData } = useTelegram();
 
 const API_HOST = import.meta.env.VITE_API_HOST || 'http://localhost:3779'
 const apiClient = createClient<paths>({
   baseUrl: API_HOST,
+  headers: {
+    'initData': webAppInitData,
+  },
 });
 
 export default apiClient;
