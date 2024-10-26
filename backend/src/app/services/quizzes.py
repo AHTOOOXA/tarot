cd010cdf6afe5f6f374776661e5162fc0b12d517
Anton Safonov <anton.safonov.loves.cats@gmail.com>
2024-10-26 10:42:36 +0500

    q

--- backend/src/app/services/quizzes.py
+++ backend/src/app/services/quizzes.py
@@ -26,16 +26,12 @@ class QuizzesService(BaseService):
         return QuizListSchema(quizzes=quizzes)
 
     async def create_quiz_response(self, quiz_response: QuizResponseSchema) -> None:
-        question_id = quiz_response.question_id
-        answer_id = quiz_response.answer_id
-
-        if not question_id or not answer_id:
-            raise ValueError("Both question_id and answer_id are required")
-
         quiz_response = await self.repo.quiz_responses.create_quiz_response(
-            taker_id=quiz_response.taker_id, question_id=question_id, answer_id=answer_id
+            taker_id=quiz_response.taker_id,
+            question_id=quiz_response.question_id,
+            answer_id=quiz_response.answer_id,
         )
-        question = await self.repo.questions.get_question_by_id(question_id)
+        question = await self.repo.questions.get_question_by_id(quiz_response.question_id)
         await self.producer.publish(
             {
                 "user_id": quiz_response.answer_id,
