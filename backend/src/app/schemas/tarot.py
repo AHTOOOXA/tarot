da47dd84df471884239d7a657a38e26dd1dfc739
Anton Safonov <anton.safonov.loves.cats@gmail.com>
2024-11-18 19:27:13 +0300

    cards repo

--- backend/src/app/schemas/tarot.py
+++ backend/src/app/schemas/tarot.py
@@ -2,16 +2,28 @@ from typing import List
 
 from pydantic import BaseModel
 
+from app.config import tgbot_config
+
 
 class TarotCard(BaseModel):
     id: int
-    name: str
-    description: str
-    image_url: str
-    keywords: List[str]
-    meaning_upright: str
-    meaning_reversed: str
-    is_reversed: bool = False
+    key: str
+
+    @property
+    def name(self) -> str:
+        from app.infrastructure.i18n import i18n
+
+        return i18n(f"cards.{self.key}.name")
+
+    @property
+    def description(self) -> str:
+        from app.infrastructure.i18n import i18n
+
+        return i18n(f"cards.{self.key}.description")
+
+    @property
+    def image_url(self) -> str:
+        return f"{tgbot_config.api_domain}/static/images/moon_magic_tarot/{self.key}.jpg"
 
 
 class DailyReadingMessage(BaseModel):
