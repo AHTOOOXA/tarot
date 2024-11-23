fea10478dcaaac6a80184dde2390a64388499dc3
Anton Safonov <anton.safonov.loves.cats@gmail.com>
2024-11-24 00:15:49 +0300

    react components (example)

---
+++ reactcomp/components/Navigation/Pagination/Pagination.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Pagination, PaginationProps } from './Pagination';
+
+const meta = {
+  title: 'Navigation/Pagination',
+  component: Pagination,
+} satisfies Meta<typeof Pagination>;
+
+export default meta;
+
+export const Playground: StoryObj<PaginationProps> = {};
