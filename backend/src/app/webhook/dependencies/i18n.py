a2a6ee82e42ca3a507c2ef1d3734b552c174ee82
Anton Safonov <anton.safonov.loves.cats@gmail.com>
2024-11-26 14:52:26 +0300

    i18n typing

--- backend/src/app/webhook/dependencies/i18n.py
+++ backend/src/app/webhook/dependencies/i18n.py
@@ -1,11 +1,13 @@
+from typing import AsyncGenerator
+
 from fastapi import Depends, Request
 
-from app.infrastructure.i18n import i18n, t
+from app.infrastructure.i18n import I18nManager, i18n
 from app.schemas.users import UserSchema
 from app.webhook.auth import get_twa_user
 
 
-async def get_i18n(request: Request, user: UserSchema = Depends(get_twa_user)):
+async def get_i18n(request: Request, user: UserSchema = Depends(get_twa_user)) -> AsyncGenerator[I18nManager, None]:
     i18n.set_user_locale(user)
     try:
         yield i18n
